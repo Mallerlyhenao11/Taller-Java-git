33e03e5903b7535854af95e82aa361b65ed5c0cd
Mallerly <mallerlyvanesahenaogallego@gmail.com>
2024-05-04 21:23:54 -0500

    Polimorfismo - Implementación en Clase Empleado por Mallerl Henao

--- Empleado.java
+++ Empleado.java
@@ -1,8 +1,13 @@
 import java.time.LocalDate;
 
-public class Empleado extends Personas {
+public class Empleado extends Personas implements Trabajador {
     
     public Empleado (int edad, String nombre, LocalDate fechaNacimiento, String grado) {
         super(edad, nombre, fechaNacimiento);
     }
+
+    public void realizarTarea (){
+        System.out.println("Realizando tarea Laboral");
+    }
+
 }
