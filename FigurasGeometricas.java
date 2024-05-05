9d43a385ee2375e35f15fe55c851b946e9e87784
Mallerly <mallerlyvanesahenaogallego@gmail.com>
2024-05-05 16:51:08 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Mallerly Henao

---
+++ FigurasGeometricas.java
@@ -0,0 +1,25 @@
+public class FigurasGeometricas {
+    private String nombre;
+    private String color;
+
+    
+    public FigurasGeometricas(String nombre, String color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+ }
+
