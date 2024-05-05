f18a0763e9f78d475049b23a277715bda44a1fb8
Mallerly <mallerlyvanesahenaogallego@gmail.com>
2024-05-04 20:59:06 -0500

    Herencia - Implementación de Constructores por Mallerly Henao

--- Estudiante.java
+++ Estudiante.java
@@ -1,7 +1,26 @@
 //mmmmm
 
+import java.time.LocalDate;
+
 public class Estudiante extends Personas {
     
     private String grado;
+
     
+    public Estudiante(int edad, String nombre, LocalDate fechaNacimiento, String grado) {
+        super(edad, nombre, fechaNacimiento);
+        this.grado = grado; 
+
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+
+    
+       
 }
