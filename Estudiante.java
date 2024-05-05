7ad1f772c9c88eb22484ccb2b56c28a0539e39ea
Mallerly <mallerlyvanesahenaogallego@gmail.com>
2024-05-04 21:06:21 -0500

    Herencia - Sobrescritura del método toString() por Mallerly Henao

--- Estudiante.java
+++ Estudiante.java
@@ -6,7 +6,6 @@ public class Estudiante extends Personas {
     
     private String grado;
 
-    
     public Estudiante(int edad, String nombre, LocalDate fechaNacimiento, String grado) {
         super(edad, nombre, fechaNacimiento);
         this.grado = grado; 
@@ -21,6 +20,10 @@ public class Estudiante extends Personas {
         this.grado = grado;
     }
 
-    
+    @Override
+    public String toString(){
+        return "El nombre del estudiante es: "+ getNombre() +" su edad es: " + getEdad()+"Su fecha de nacimiento es: " + getFechaNacimiento()+ " y por ultimo esta cursando el grado: " + getGrado();
+
+    }
        
 }
