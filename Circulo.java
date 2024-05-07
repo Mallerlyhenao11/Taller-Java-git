49e326b99955969f6baf3477e0c803caf6f118d1
Mallerly <mallerlyvanesahenaogallego@gmail.com>
2024-05-07 08:14:44 -0500

    NUEVO-Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Mallerly Henao

---
+++ Circulo.java
@@ -0,0 +1,21 @@
+public class Circulo  {
+
+    private Float pi;
+    private Float radio;
+
+  
+    public Float getPi() {
+        return pi;
+    }
+    public void setPi(Float pi) {
+        this.pi = pi;
+    }
+    public Float getRadio() {
+        return radio;
+    }
+    public void setRadio(Float radio) {
+        this.radio = radio;
+    }
+
+    
+}
---
+++ Rectangulo.java
@@ -0,0 +1,21 @@
+public class Rectangulo {
+
+    private Float base;
+    private Float altura;
+
+    
+    public Float getBase() {
+        return base;
+    }
+    public void setBase(Float base) {
+        this.base = base;
+    }
+    public Float getAltura() {
+        return altura;
+    }
+    public void setAltura(Float altura) {
+        this.altura = altura;
+    }
+
+    
+}
